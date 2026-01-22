99497b37bfc94778a4de82593c70929f0708ccb8
anhle1-tw <anh.le1@wmprojects.org>
2026-01-22 17:49:15 +0700

    feat: handle error rotate list of no member
    Co-authored-by: hang.le@thoughtworks.com; nam.nguyen@thoughtworks.com

--- solution/src/test/kotlin/TeamRotator.kt
+++ solution/src/test/kotlin/TeamRotator.kt
@@ -1,4 +1,11 @@
-class TeamRotator(val membersList: List<Member>) {
+class TeamRotator {
+
+    val membersList: List<Member>
+
+    constructor(membersList: List<Member>) {
+        if(membersList.isEmpty()) throw IllegalArgumentException("Cannot rotate list of no member")
+        this.membersList = membersList
+    }
 
     fun memberList(): List<Member> {
         return membersList;
