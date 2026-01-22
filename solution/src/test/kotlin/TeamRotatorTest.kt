9143881eecea05eb452e32cdd49111b970f2e5a5
anhle1-tw <anh.le1@wmprojects.org>
2026-01-22 17:34:22 +0700

    test: add a new tests for list of members
    Co-authored-by: hang.le@thoughtworks.com; nam.nguyen@thoughtworks.com

--- solution/src/test/kotlin/TeamRotatorTest.kt
+++ solution/src/test/kotlin/TeamRotatorTest.kt
@@ -2,7 +2,15 @@ import org.junit.jupiter.api.Test
 
 class TeamRotatorTest {
     @Test
-    fun `team rotator can have a list of members`() {
+    fun `team rotator can have a list of 1 member`() {
+        val membersList = listOf(Member("AnhLe"));
+        val teamRotator = TeamRotator(membersList);
+
+        teamRotator.memberList() == listOf(Member("AnhLe"))
+    }
+
+    @Test
+    fun `team rotator can have a list of 2 members`() {
         val membersList = listOf(Member("AnhLe"), Member("Nam"));
         val teamRotator = TeamRotator(membersList);
 
