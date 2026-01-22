d39d654af6c81ac1e1f7951264e33ccc98583266
anhle1-tw <anh.le1@wmprojects.org>
2026-01-22 17:46:44 +0700

    test: add test team rotator cannot have list of no members
    Co-authored-by: hang.le@thoughtworks.com; nam.nguyen@thoughtworks.com

--- solution/src/test/kotlin/TeamRotatorTest.kt
+++ solution/src/test/kotlin/TeamRotatorTest.kt
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class TeamRotatorTest {
     @Test
@@ -21,5 +23,17 @@ class TeamRotatorTest {
         assertEquals(
             teamRotator.memberList(),
             listOf(Member("AnhLe"), Member("Nam")));
+    }
+
+    @Test
+    fun `team rotator cannot have a list of no member`() {
+        val membersList = listOf<Member>();
+
+        val error = assertFailsWith<IllegalArgumentException>(
+            block = {
+                val teamRotator = TeamRotator(membersList)
+            }
+        )
+        assertEquals("Cannot rotate list of no member", error.message)
     }
 }
